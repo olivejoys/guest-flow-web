3a788076ce4ec05574a222b0875ec6fa4f3e30fa
olivejoys <olivejoy@sheridancollege.ca>
2024-09-13 13:57:54 -0400

    feat: created full table.

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Search } from 'lucide-react'
+import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from 'lucide-react'
 
 export function AttendeeList() {
    return(
-      <div>
+      <div className="flex flex-col gap-4">
 
-      <div className="flex items-center">
+      <div className="flex gap-3  items-center">
        <h1 className="text-2xl font-bold">Attendees</h1> 
        <div className="px-3  w-72 py-1.5 border border-white/10 
        rounded-lg text-sm flex item-center gap-3" >
@@ -16,51 +16,100 @@ export function AttendeeList() {
    </div>
       </div>
 
-      <table className="w-full border border-white/10 rounded-lg">
+<div className="border border-white/10 rounded-lg">
+
+      <table className="w-full ">
          <thead>
-            <tr>
-               <th>
+            <tr className="border-b border-white/10">
+               <th style={{ width: 48 }}
+               className="py-3 px-4 text-sm font-semibold text-left">
             <input type="checkbox"/>
             </th>
-            <th>Code</th>
-            <th>Attendee</th>
-            <th>Subscription Date</th>
-            <th>Check-in Date</th>
+            <th className="py-3 px-4 text-sm font-semibold 
+            text-left">Code</th>
+            <th className="py-3 px-4 text-sm font-semibold text-left
+            ">Attendee</th>
+            <th className="py-3 px-4 text-sm font-semibold text-left"
+            >Subscription Date</th>
+            <th style={{ width: 64 }}
+             className="py-3 px-4 text-sm font-semibold text-left">
+            Check-in Date</th>
 
             </tr>
          </thead>
          <tbody>
-            <tr>
-               <td>
+           {Array.from({length: 10}).map((_, i ) => {
+            return(
+               <tr key={i} className="border-b border-white/10">
+               <td className="py-3 px-4 text-sm text-zinc-300">
             <input type="checkbox"/>
             </td>
-            <td>1283
+            <td className="py-3 px-4 text-sm text-zinc-300">1283</td>
+            <td className="py-3 px-4 text-sm text-zinc-300">
+
 
            
-            <div>
-               <span>John Wats</span>
+            <div className="flex flex-col gap-1">
+               <span className="font-semibold text-white">John Wats</span>
                <span>JohnWats@email.com</span>
             </div>
             </td>
-            <td>7 days ago</td>
-            <td>3 days ago</td>
-            <td></td>
+            <td className="py-3 px-4 text-sm text-zinc-300">7 days ago</td>
+            <td className="py-3 px-4 text-sm text-zinc-300">3 days ago</td>
+            <td className="py-3 px-4 text-sm text-zinc-300">
+               <button className="bg-black/20 border border-white/10 
+               rounded-md p-1.5">
+               <MoreHorizontal className="size-4"></MoreHorizontal>
+               </button>
+            </td>
+
             </tr>
+            )
+           })}
          </tbody>
          <tfoot>
-            <td colSpan={3}>
+            <tr>            
+            <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
             Showing 10 of 228 items 
             </td>
-            <td colSpan={3}>
-               Page 1 of 23
+            <td className="py-3 px-4 text-sm
+             text-zinc-300 text-right" colSpan={3}>
+               <div className="inline-flex items-center  gap-8">
+               <span>Page 1 of 23</span>
+
+              
+            <div className="flex gap-1.5">
+            <button className="bg-white/10 border border-white/10 
+               rounded-md p-1.5">
+               <ChevronsLeft className="size-4"></ChevronsLeft>
+               </button>
+               <button className="bg-white/10  border border-white/10 
+               rounded-md p-1.5">
+               <ChevronLeft className="size-4"></ChevronLeft>
+               </button>
+               <button className="bg-white/10  border border-white/10 
+               rounded-md p-1.5">
+               <ChevronRight className="size-4"></ChevronRight>
+               </button>
+               <button className="bg-white/10  border border-white/10 
+               rounded-md p-1.5">
+               <ChevronsRight className="size-4"></ChevronsRight>
+               </button>
+            </div>
+            </div>
+
+
+
+
             </td>
+        </tr>
 
          </tfoot>
       </table>
       </div>
 
+      </div>
 
-      
 
 
    );
