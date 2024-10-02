1ce864aa8ba21f8204fc881e3d560ad5aa51fa9a
olivejoys <olivejoy@sheridancollege.ca>
2024-10-02 12:31:52 -0400

    created table-row component

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -3,6 +3,8 @@ import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, Chevro
 import { IconButton } from "./icon-button";
 import { Table } from "../table/table";
 import { TableHeader } from "../table/table-header";
+import { TableCell } from "../table/table-cell";
+import { TableRow } from "../table/table-roll";
 
 export function AttendeeList() {
    return (
@@ -41,8 +43,8 @@ export function AttendeeList() {
                <tbody>
                   {Array.from({ length: 10 }).map((_, i) => {
                      return (
-                        <tr key={i} className="border-b border-white/10 hover:bg-white/5">
-                           <td className="py-3 px-4 text-sm text-zinc-300">
+                        <TableRow key={i}>
+                           <TableCell>
                               <div className="peer">
                                  <input type="checkbox" id={`checkbox-${i}`} className="peer hidden" />
                                  <label
@@ -50,33 +52,33 @@ export function AttendeeList() {
                                     className="inline-block w-4 h-4 bg-black/20 rounded border border-white/10 peer-checked:bg-orange-400"
                                  ></label>
                               </div>
-                           </td>
-                           <td className="py-3 px-4 text-sm text-zinc-300">1283</td>
-                           <td className="py-3 px-4 text-sm text-zinc-300">
+                           </TableCell>
+                           <TableCell>1283</TableCell>
+                           <TableCell>
                               <div className="flex flex-col gap-1">
-                                 <span className="font-semibold text-white">John Wats</span>
+                                 <span className="font-semibold
+                                  text-white">John Wats</span>
                                  <span>JohnWats@email.com</span>
                               </div>
-                           </td>
-                           <td className="py-3 px-4 text-sm text-zinc-300"
-                           >7 days ago</td>
-                           <td className="py-3 px-4 text-sm text-zinc-300"
-                           >3 days ago</td>
-                           <td className="py-3 px-4 text-sm text-zinc-300">
+                           </TableCell>
+                           <TableCell>7 days ago</TableCell>
+                           <TableCell>3 days ago</TableCell>
+                           <TableCell>
                               <IconButton transparent>
                                  <MoreHorizontal className="size-4" />
                               </IconButton>
-                           </td>
-                        </tr>
+                           </TableCell>
+                        </TableRow>
                      );
                   })}
                </tbody>
                <tfoot>
                   <tr>
-                     <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
+                     <TableCell className="py-3 px-4 text-sm
+                      text-zinc-300" colSpan={3}>
                         Showing 10 of 228 items
-                     </td>
-                     <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
+                     </TableCell>
+                     <TableCell className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
                         <div className="inline-flex items-center gap-8">
                            <span>Page 1 of 23</span>
                            <div className="flex gap-1.5">
@@ -94,7 +96,7 @@ export function AttendeeList() {
                               </IconButton>
                            </div>
                         </div>
-                     </td>
+                     </TableCell>
                   </tr>
                </tfoot>
             </Table>
