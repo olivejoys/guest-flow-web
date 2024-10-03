d038a5767740befbc66d2bc7fdaf31c15cdc3f20
olivejoys <olivejoy@sheridancollege.ca>
2024-10-03 14:15:46 -0400

    created const page to start from page 1 and show 10 attendees only.

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -15,7 +15,7 @@ dayjs.extend(relativeTime);
 
 export function AttendeeList() {
    const [search, setSearch] = useState('')
-   
+   const [page, setPage] = useState(1)
    function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
       setSearch(event.target.value)
       
@@ -57,7 +57,7 @@ export function AttendeeList() {
                   </tr>
                </thead>
                <tbody>
-                  {attendees.map((attendee) => {
+                  {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
                      return (
                         <TableRow key={attendee.id}>
                            <TableCell>
